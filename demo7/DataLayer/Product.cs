6aff4c355035287f173c99e2b163c673441016f3
Alexandru Bodnar <alexandrubodnar10@gmail.com>
2018-11-16 10:17:59 +0200

    DataLayer and BusinessLayer

---
+++ demo7/DataLayer/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataLayer
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { set; get; }
+        public string Name { set; get; }
+        public double Price { set; get; }
+        public int Pieces { set; get; }
+        public double Total { private set; get; }
+
+        public Guid ShoppingCartId { set; get; }
+        public ShoppingCart ShoppingCart { set; get; }
+
+        public Product()
+        {
+            Total = Price * Pieces;
+        }
+
+
+    }
+}
