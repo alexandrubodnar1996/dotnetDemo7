01d404820ac029234138e9279102af12841a7b15
Marilena Dascalu <marilenadascalu97@yahoo.com>
2018-11-16 11:41:05 +0200

    Add files via upload

---
+++ Product Repo Controller/ProductRepository.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using DataLayer;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLayer
+{
+	public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationContext _appContext;
+
+        public ProductRepository(ApplicationContext applicationContext)
+        {
+            _appContext = applicationContext;
+        }
+
+        public Guid Create()
+        {
+            Product product = new Product();
+            return product.Id;
+        }
+
+		public int GetById(Guid ProductId)
+        {
+            Product product = _appContext.Set<Product>().Where(p => p.Id == ProductId).FirstOrDefault();
+            return product;
+        }
+
+		public void RemoveById(Guid ProductId)
+        {
+            Products.Remove(Products.First(p => p.Id == guid));
+        }
+
+    }
+}
